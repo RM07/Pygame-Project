a12eb29b5f6eaa07f85d4be01e6b3df75fe79c05
RM07 <rohanmeena07@iitkgp.ac.in>
2019-03-21 01:41:45 +0530

    Random generation of patter code added

--- Codeclub.py
+++ Codeclub.py
@@ -8,7 +8,7 @@ FLASHSPEED = 500
 FLASHDELAY = 200
 BUTTONSIZE = 200
 BUTTONGAPSIZE = 20
-TIMEOUT =4
+TIMEOUT = 4
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -36,3 +36,59 @@ def main():
     global FPSCLOCK, DISPLAYSURF ,BASICFONT , BEEP1, BEEP2, BEEP3, BEEP4
 
     pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
+    pygame.display.set_caption("CODECLUB")
+
+    BASICFONT = pygame.font.Font("freesansbold.ttf", 16)
+
+    infoSurf = BASICFONT.render("Please Match the Pattern by Clicking Button or Using the Q,W,A,S keys",1,DARKGRAY)
+    infoRect = infoSurf.get_rect()
+    infoRect.topleft = (10, WINDOWHEIGHT-25)
+    BEEP1 = pygame.mixer.Sound("beep1.ogg")
+    BEEP2 = pygame.mixer.Sound("beep2.ogg")
+    BEEP3 = pygame.mixer.Sound("beep3.ogg")
+    BEEP4 = pygame.mixer.Sound("beep4.ogg")
+    
+    pattern = []
+    currentStep = 0
+    lastClickTime = 0
+    score = 0
+
+    waitingForInput = False
+
+    while True:
+        clickedButton = None
+        DISPLAYSURF.fill(bgColor)
+        drawButtons()
+
+        ScoreSurf = BASICFONT.render("Score: " + str(score),1,WHITE)
+        scoreRect = scoreSurf.get_rect()
+        scoreRect.topleft = (WINDOWWIDTH-100,10)
+        DISPLAYSURF.blit(scoreSurf,scoreRect)
+        DISPLAYSURF.blit(infoSurf,infoRect)
+        checkForQuit()
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                mousex , mousey = event.pos
+                clickedButton = getButtonClicked(nousex,mousey)
+            elif event.type == KEYDOWN:
+                if event.key == K_q:
+                    clickedButton = YELLOW
+                elif event.key == k_w:
+                    clickedButton = BLUE
+                elif event.key == k_a:
+                    clickedButton = RED
+                elif event.key = k_s:
+                    clickedButton = GREEN
+        if not waitingForInput:
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pattern.append(random.choice(YELLOW,BLUE,RED,GREEN))
+            for button in pattern:
+                flashButtonAnimation(button)
+                pygame.time.wait(FLASHDELAY)
+            waitingForInput = True
+
+
+
